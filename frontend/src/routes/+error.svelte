29a16d8503dab0c127bca96a71d327755252ed46
Matyanson <matyas.perem@seznam.cz>
2025-11-07 19:12:12 +0100

    feat(frontend): integrate keycloak

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<h1>Error from home page: {page.status}</h1>
+<p>{page.error?.message}</p>
--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,18 +1,33 @@
-import { UserManagementApi, type HealthCheckResponse } from "$lib/api";
+import { Configuration, FetchError, SettingsServiceApi, UserManagementApi, type HealthCheckResponse } from "$lib/api";
+import { initKeycloak, keycloak } from "$lib/auth/keycloak";
+import { error, redirect } from "@sveltejs/kit";
+
 import type { LayoutLoad } from "./$types";
 
-export const prerender = true;
+export const prerender = false;
 export const ssr = false;
 
-export const load: LayoutLoad = async () => {
+export const load: LayoutLoad = async ({ fetch }) => {
+  console.log("Layout load started");
+  await initKeycloak();
+  console.log("Layout load finished");
 
-    const client = new UserManagementApi();
-    // const health = await client.getHealthCheck();
-    const health: HealthCheckResponse = { status: "mock" }
+  if (!keycloak.authenticated) {
+    console.warn("User not authenticated, redirecting to login...");
+    throw redirect(302, await keycloak.createLoginUrl());
+  }
 
-    return {
-        test: 'hello',
-        world: 'world',
-        health
-    };
+  console.log("✅ Keycloak authenticated, token:", keycloak.token?.substring(0, 10) + "...");
+
+  const config = new Configuration({
+    accessToken: async () => `Bearer ${keycloak.token ?? ""}`,
+    fetchApi: fetch
+  });
+
+  const clients = {
+    management: new UserManagementApi(config),
+    settings: new SettingsServiceApi(config)
+  };
+
+  return { clients };
 };
