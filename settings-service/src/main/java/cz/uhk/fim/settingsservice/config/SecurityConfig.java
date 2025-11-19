2394bcfae2c6ec656ef0410569b86c587cfb4328
Matěj Váňa <15677805+Acerik@users.noreply.github.com>
2025-11-19 15:04:28 +0100

    feat: add second international student registration (#41)
    * feat: add second international student registration
    
    * fix: pr changes
    
    * fix: remove security for settings get faculties

--- settings-service/src/main/java/cz/uhk/fim/settingsservice/config/SecurityConfig.java
+++ settings-service/src/main/java/cz/uhk/fim/settingsservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package cz.uhk.fim.settingsservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/health").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/faculty").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
