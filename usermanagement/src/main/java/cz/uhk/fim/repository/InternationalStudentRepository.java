29981108667973f816a6f7fb529ebdec6ead4a9f
Matěj Váňa <15677805+Acerik@users.noreply.github.com>
2025-11-22 00:53:25 +0100

    feat: add EP for dashboard international students (#42)
    * feat: add EP for dashboard international students
    
    * feat: add EP for dashboard international students

--- usermanagement/src/main/java/cz/uhk/fim/repository/InternationalStudentRepository.java
+++ usermanagement/src/main/java/cz/uhk/fim/repository/InternationalStudentRepository.java
@@ -1,11 +1,24 @@
 package cz.uhk.fim.repository;
 
 import cz.uhk.fim.entity.InternationalStudentEntity;
+import jakarta.annotation.Nullable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
 @Repository
 public interface InternationalStudentRepository extends JpaRepository<InternationalStudentEntity, UUID> {
 
+
+    @Query(""" 
+            select student from InternationalStudentEntity student where 
+                                 (:facultyId is null or student.facultyId = :facultyId) and
+                                 (:semesterId is null or student.semesterId = :semesterId) and
+                                 (:countryCode is null or student.countryISO = :countryCode)
+            """)
+    Page<InternationalStudentEntity> findAllByFilters(Pageable pageable, @Nullable UUID semesterId, @Nullable UUID facultyId, @Nullable String countryCode);
 }
