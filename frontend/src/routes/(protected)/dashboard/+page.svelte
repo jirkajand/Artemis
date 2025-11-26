da6690a66e5dac038e9b9ec14053ca3ac3ca4315
doesntmatter123 <49456194+doesntmatter123@users.noreply.github.com>
2025-11-26 23:46:44 +0100

    pagination - partial fetch (#62)

--- frontend/src/routes/(protected)/dashboard/+page.svelte
+++ frontend/src/routes/(protected)/dashboard/+page.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
 	import AnonymisedStudentCard from '$lib/components/AnonymisedStudentCard.svelte';
-	import { invalidateAll } from '$app/navigation';
+	import { goto } from '$app/navigation';
 
 	let { data } = $props();
-	let { management, students } = $derived(data);
+	let { students , pagination , management } = $derived(data);
 
 	async function handleBuddyMatch(student: any) {
-		console.log('Processing match for:', student.id);
 		try {
 			await management.assignInternationalStudentToLocalStudent({ internationalStudentId: student.id });
 			alert(`You are now a buddy for this student from ${student.countryName}!`);
-			await invalidateAll();
-
+			await goto(location.href, { invalidateAll: true });
 		} catch (e) {
 			alert(`Something went wrong!`);
 		}
 	}
 </script>
 
-{#each students as student, i (i)}
-	<AnonymisedStudentCard {student} onPick="{() => handleBuddyMatch(student)}" />
+{#each students as student (student.id)}
+	<AnonymisedStudentCard {student} onPick={() => handleBuddyMatch(student)} />
 {/each}
-
-<style>
-</style>
--- frontend/src/routes/(protected)/my-students/+page.svelte
+++ frontend/src/routes/(protected)/my-students/+page.svelte
@@ -5,17 +5,6 @@
 	let { data } = $props();
 	let { management, students } = $derived(data);
 
-	async function handleBuddyMatch(student: any) {
-		console.log("Processing match for:", student.id);
-		try{
-			await management.assignInternationalStudentToLocalStudent({internationalStudentId: student.id})
-			alert(`You are now a buddy for this student from ${student.countryName}!`);
-			await invalidateAll()
-
-		} catch (e) {
-			alert(`Something went wrong! Cause: ${e}`);
-		}
-	}
 </script>
 
 {#each students as student, i (i)}
