5d9eb7c7957702eeaa99cba2fd440f75c18b06ed
Matyanson <matyas.perem@seznam.cz>
2025-12-02 00:26:49 +0100

    feat: add country codes input

---
+++ frontend/src/lib/components/registration/CountryCodeInput.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Autocomplete from "@smui-extra/autocomplete";
+    import { countryIsoCodes } from "$lib/assets/country-iso-codes";
+
+    let { value = $bindable(), ...rest } = $props();
+</script>
+
+
+<Autocomplete bind:value={value} {...rest} options={countryIsoCodes} />
