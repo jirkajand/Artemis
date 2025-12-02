e07b10f31cd7e4e23ff49bab98f7b1e29eb22cca
Matyanson <matyas.perem@seznam.cz>
2025-12-02 01:08:22 +0100

    Feat/primary international student registration (#84)
    * feat: add country codes input
    
    * feat: add phone code input
    
    * feat: added international students primary registration
    
    * fix: do not modify reactive form values
    
    * chore: minor changes
    
    * fix: add missing country-iso codes
    
    * fix(register): switch international and member positions

---
+++ frontend/src/lib/components/registration/CountryCodeInput.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Autocomplete from "@smui-extra/autocomplete";
+    import { countryIsoCodes } from "$lib/assets/country-iso-codes";
+
+    let { value = $bindable(), ...rest } = $props();
+</script>
+
+
+<Autocomplete bind:value={value} {...rest} options={countryIsoCodes} />
