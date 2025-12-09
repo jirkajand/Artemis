917999221f9f5686b87eb0ad9c6f76369331d050
Matyanson <matyas.perem@seznam.cz>
2025-12-09 18:12:45 +0100

    fix(admin): chose initial active tab based on url

--- frontend/src/routes/(protected)/admin/+layout.svelte
+++ frontend/src/routes/(protected)/admin/+layout.svelte
@@ -2,6 +2,7 @@
 	import Tab, { Label } from "@smui/tab";
 	import TabBar from "@smui/tab-bar";
 	import type { LayoutProps } from "./$types";
+	import { page } from "$app/state";
 
   let { children }: LayoutProps = $props();
 
@@ -10,10 +11,12 @@
         'Faculties': { slug: 'faculty'},
     }
     const tabKeys = Object.keys(tabDict);
+    // dynamicaly determine active tab based on current page
+    const activeTab = tabKeys.find(key => page.url.pathname.includes(tabDict[key].slug)) || tabKeys[0];
 
 </script>
 
-<TabBar style="margin-bottom: 1.5rem" tabs={tabKeys} active={tabKeys[0]}>
+<TabBar style="margin-bottom: 1.5rem" tabs={tabKeys} active={activeTab}>
     {#snippet tab(tab)}
       <Tab {tab} href={`/admin/${tabDict[tab].slug}`}>
         <Label>{tab}</Label>
